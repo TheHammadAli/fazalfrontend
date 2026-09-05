c6218aba5f6327ae2845afc35fca586e50e81f08
Aliyan Sajid <hammadalimughal08@gmail.com>
2026-09-05 18:01:56 +0500

    updated code

--- components/Selling/ShopDetail.tsx
+++ components/Selling/ShopDetail.tsx
@@ -362,7 +362,7 @@ export default function ShopDetail() {
                 </div> */}
 
                 {isShopOwner ? (
-                  <div className="mt-4 flex flex-wrap items-center justify-between gap-2">
+                  <div className="mt-4 flex flex-wrap items-center gap-2">
                     <DoodleButton
                       onClick={() =>
                         router.push(`/selling/list-product?id=${shop?.data?.id}`)
